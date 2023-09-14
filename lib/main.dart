2a9647bad1d1ab0d36eb851914037d7a55c6e94c
Rahmat-09 <jrahmat507@gmail.com>
2023-09-14 08:43:37 +0700

    update map

--- lib/main.dart
+++ lib/main.dart
@@ -23,58 +23,39 @@ Future main() async {
     return true;
   };
 
-
   // final fcmToken = await FirebaseMessaging.instance.getToken();
-
 
   dynamic token = SessionManager().get("token");
 
   runApp(MaterialApp(
-    home: token!=''? LayoutNavigationBar(accessToken: token) : const MyApp(),
+    title: 'Kinton Driver',
+    theme: ThemeData(
+      primaryColor: HexColor("#ef9904"),
+      // This is the theme of your application.
+      //
+      // TRY THIS: Try running your application with "flutter run". You'll see
+      // the application has a blue toolbar. Then, without quitting the app,
+      // try changing the seedColor in the colorScheme below to Colors.green
+      // and then invoke "hot reload" (save your changes or press the "hot
+      // reload" button in a Flutter-supported IDE, or press "r" if you used
+      // the command line to start the app).
+      //
+      // Notice that the counter didn't reset back to zero; the application
+      // state is not lost during the reload. To reset the state, use hot
+      // restart instead.
+      //
+      // This works for code too, not just values: Most code changes can be
+      // tested with just a hot reload.
+      colorScheme: ColorScheme.fromSeed(seedColor: HexColor("#ef9904")),
+      useMaterial3: true,
+    ),
+    home: token!=''? LayoutNavigationBar(accessToken: token) : const LoginPage(),
   ));
 
 }
 
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
 
-class _MyAppState extends State<MyApp> {
 
-  Future<void> checkLogin() async {
-
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Kinton Driver',
-      theme: ThemeData(
-        primaryColor: HexColor("#ef9904"),
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: HexColor("#ef9904")),
-        useMaterial3: true,
-      ),
-      home: const LoginPage()
-    );
-  }
-}
 
